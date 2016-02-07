1e653a5fc0463a3ae6d29d28cf5b23635d9aa409
Brian Scramlin <scramlo@hotmail.com>
2016-02-07 16:15:40 -0600

    Updated Profile Show View and Created links to profiles via My Team page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,9 @@
 class StaticController < ApplicationController
   def user
     @profile = Profile.where(user_id: current_user.id).take
+    @prayers = Prayer.where(user_id: current_user.id)
+    @praises = Praise.where(user_id: current_user.id)
+    @answered = Prayer.where(user_id: current_user.id).where(answered: TRUE)
   end
 
   def about
